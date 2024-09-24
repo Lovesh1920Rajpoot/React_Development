f6dcae9d05ae8cd0d77e106ee9aa20f6d6c06627
Lovesh1920Rajpoot <91554076+Lovesh1920Rajpoot@users.noreply.github.com>
2024-09-24 17:29:30 +0530

    Add files via upload

---
+++ brandpage/App.js
@@ -0,0 +1,14 @@
+import Navigator from "./components/Navigator";
+import "./App.css";
+import Hero from "./components/Hero";
+
+function App() {
+  return (
+    <div>   
+      <Navigator/>
+      <Hero/>
+    </div>
+  );
+}
+
+export default App;

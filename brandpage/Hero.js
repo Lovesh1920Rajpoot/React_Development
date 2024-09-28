b23814eb2a93fb241425aa33f045da23bf499ab3
React_Development <loveshkumar2000mahur@gmail.com>
2024-09-28 20:24:06 +0530

    Change In Heading

--- brandpage/Hero.js
+++ brandpage/Hero.js
@@ -4,7 +4,7 @@ export default function Hero() {
   return (
     <main className="Hero container">
        <div className="Hero-content">
-            <h1>YOUR FEET DESERVE THE BEST</h1>   
+            <h1>YOUR FEET DESERVE THE BEST Resource</h1>   
             <p>YOUR FEET DESERVE THE BEST AND 
             WE’RE HERE TO HELP YOU WITH OUR SHOES.
             YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU 

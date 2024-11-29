541e962da9d98149b778a6873936cc4c2d36fe11
Lovesh1920Rajpoot <91554076+Lovesh1920Rajpoot@users.noreply.github.com>
2024-11-29 21:54:49 +0530

    Add files via upload

---
+++ Jiraclone/App.js
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import Board from "./Componenets/Board";
+
+
+function App() {
+  return (
+    <div>
+      <Board/>
+    </div>
+  );
+}
+
+export default App;
